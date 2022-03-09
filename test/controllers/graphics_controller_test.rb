5abf35d962cf56aac43e856aecfdb87d2e357ad6
douglasconstancio <douglas.constancio@trustedhealth.com>
2022-03-09 13:25:04 -0300

    feat: Add graphics controller

---
+++ test/controllers/graphics_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class GraphicsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get graphics_index_url
+    assert_response :success
+  end
+end
